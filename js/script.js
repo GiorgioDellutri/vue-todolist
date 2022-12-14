b67738495a61867e0de97abec9e0804af5f695f8
GiorgioDellutri <giorgiodll93@gmail.com>
2022-12-14 13:10:36 +0100

    script

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const {createApp} = Vue; 
+
+createApp({
+    data() {
+        return{
+            activeIndex: 0,
+            slides : [
+                    {
+                        text: '',
+                        done:   
+                    }, 
+                    {
+                        text: '',
+                        done:  
+                    },
+                    {
+                        text: '',
+                        done:  
+                    },
+                    {
+                        text: '',
+                        done:  
+                    },
+                    {
+                        text: '',
+                        done:  
+                    },
+                ],
+        }
+    },
+
+    methods: {
+    }
+}).mount('#app')
+
